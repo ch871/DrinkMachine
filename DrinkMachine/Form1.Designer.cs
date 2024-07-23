71bbdc749741a0a85526dd4470e6762f900555c7
chaim <shterenchaim@gmail.com>
2024-07-23 12:16:23 +0300

    Add project files.

---
+++ DrinkMachine/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace DrinkMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDrinksTable = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.suger = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.coffee = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cocoa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.milk = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cmbSugar = new System.Windows.Forms.ComboBox();
+            this.cmbCocou = new System.Windows.Forms.ComboBox();
+            this.cmbMilk = new System.Windows.Forms.ComboBox();
+            this.cmbCffe = new System.Windows.Forms.ComboBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNewDrinke = new System.Windows.Forms.TextBox();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnAddDrink = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDrinksTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDrinksTable
+            // 
+            this.dgvDrinksTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDrinksTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.suger,
+            this.coffee,
+            this.cocoa,
+            this.milk,
+            this.price});
+            this.dgvDrinksTable.Location = new System.Drawing.Point(151, 271);
+            this.dgvDrinksTable.Name = "dgvDrinksTable";
+            this.dgvDrinksTable.RowHeadersWidth = 62;
+            this.dgvDrinksTable.RowTemplate.Height = 28;
+            this.dgvDrinksTable.Size = new System.Drawing.Size(643, 267);
+            this.dgvDrinksTable.TabIndex = 0;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "שם המשקה";
+            this.name.MinimumWidth = 8;
+            this.name.Name = "name";
+            this.name.Width = 150;
+            // 
+            // suger
+            // 
+            this.suger.HeaderText = "סוכר";
+            this.suger.MinimumWidth = 8;
+            this.suger.Name = "suger";
+            this.suger.Width = 50;
+            // 
+            // coffee
+            // 
+            this.coffee.HeaderText = "קפה";
+            this.coffee.MinimumWidth = 8;
+            this.coffee.Name = "coffee";
+            this.coffee.Width = 50;
+            // 
+            // cocoa
+            // 
+            this.cocoa.HeaderText = "שוקו";
+            this.cocoa.MinimumWidth = 8;
+            this.cocoa.Name = "cocoa";
+            this.cocoa.Width = 50;
+            // 
+            // milk
+            // 
+            this.milk.HeaderText = "חלב";
+            this.milk.MinimumWidth = 8;
+            this.milk.Name = "milk";
+            this.milk.Width = 50;
+            // 
+            // price
+            // 
+            this.price.HeaderText = "מחיר";
+            this.price.MinimumWidth = 8;
+            this.price.Name = "price";
+            this.price.Width = 50;
+            // 
+            // cmbSugar
+            // 
+            this.cmbSugar.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbSugar.FormattingEnabled = true;
+            this.cmbSugar.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cmbSugar.Location = new System.Drawing.Point(770, 207);
+            this.cmbSugar.Name = "cmbSugar";
+            this.cmbSugar.Size = new System.Drawing.Size(106, 37);
+            this.cmbSugar.TabIndex = 1;
+            // 
+            // cmbCocou
+            // 
+            this.cmbCocou.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCocou.FormattingEnabled = true;
+            this.cmbCocou.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cmbCocou.Location = new System.Drawing.Point(452, 207);
+            this.cmbCocou.Name = "cmbCocou";
+            this.cmbCocou.Size = new System.Drawing.Size(118, 37);
+            this.cmbCocou.TabIndex = 2;
+            // 
+            // cmbMilk
+            // 
+            this.cmbMilk.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbMilk.FormattingEnabled = true;
+            this.cmbMilk.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cmbMilk.Location = new System.Drawing.Point(276, 207);
+            this.cmbMilk.Name = "cmbMilk";
+            this.cmbMilk.Size = new System.Drawing.Size(140, 37);
+            this.cmbMilk.TabIndex = 3;
+            // 
+            // cmbCffe
+            // 
+            this.cmbCffe.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCffe.FormattingEnabled = true;
+            this.cmbCffe.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.cmbCffe.Location = new System.Drawing.Point(618, 207);
+            this.cmbCffe.Name = "cmbCffe";
+            this.cmbCffe.Size = new System.Drawing.Size(117, 37);
+            this.cmbCffe.TabIndex = 4;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(351, 81);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(165, 52);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "עדכון סוג";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(136, 81);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(186, 52);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "שמירה";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(735, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "סוג שתיה חדש";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(177, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 29);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "מחיר";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(310, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 29);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "חלב";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(479, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 29);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "קקאו";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(626, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 29);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "קפה";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(787, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 29);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "סוכר";
+            // 
+            // txtNewDrinke
+            // 
+            this.txtNewDrinke.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNewDrinke.Location = new System.Drawing.Point(551, 81);
+            this.txtNewDrinke.Multiline = true;
+            this.txtNewDrinke.Name = "txtNewDrinke";
+            this.txtNewDrinke.Size = new System.Drawing.Size(162, 42);
+            this.txtNewDrinke.TabIndex = 14;
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.BackColor = System.Drawing.Color.Red;
+            this.btnMinus.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinus.Location = new System.Drawing.Point(12, 211);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(49, 44);
+            this.btnMinus.TabIndex = 15;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = false;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.BackColor = System.Drawing.Color.Lime;
+            this.btnPlus.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlus.Location = new System.Drawing.Point(67, 211);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(49, 44);
+            this.btnPlus.TabIndex = 16;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = false;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnAddDrink
+            // 
+            this.btnAddDrink.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnAddDrink.Location = new System.Drawing.Point(351, 26);
+            this.btnAddDrink.Name = "btnAddDrink";
+            this.btnAddDrink.Size = new System.Drawing.Size(165, 49);
+            this.btnAddDrink.TabIndex = 17;
+            this.btnAddDrink.Text = "הוספת סוג";
+            this.btnAddDrink.UseVisualStyleBackColor = true;
+            this.btnAddDrink.Click += new System.EventHandler(this.btnAddDrink_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnDelete.Location = new System.Drawing.Point(136, 26);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(186, 49);
+            this.btnDelete.TabIndex = 18;
+            this.btnDelete.Text = "מחיקה";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Font = new System.Drawing.Font("MS Reference Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrice.Location = new System.Drawing.Point(136, 207);
+            this.txtPrice.Multiline = true;
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.ReadOnly = true;
+            this.txtPrice.Size = new System.Drawing.Size(134, 48);
+            this.txtPrice.TabIndex = 19;
+            this.txtPrice.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 532);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddDrink);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.txtNewDrinke);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.cmbCffe);
+            this.Controls.Add(this.cmbMilk);
+            this.Controls.Add(this.cmbCocou);
+            this.Controls.Add(this.cmbSugar);
+            this.Controls.Add(this.dgvDrinksTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDrinksTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDrinksTable;
+        private System.Windows.Forms.ComboBox cmbSugar;
+        private System.Windows.Forms.ComboBox cmbCocou;
+        private System.Windows.Forms.ComboBox cmbMilk;
+        private System.Windows.Forms.ComboBox cmbCffe;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNewDrinke;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnAddDrink;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn suger;
+        private System.Windows.Forms.DataGridViewTextBoxColumn coffee;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cocoa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn milk;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price;
+    }
+}
+
---
+++ DrinkMachine/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace DrinkMachine
+{
+
+    public partial class Form1 : Form
+    {
+        //יצירת משתנה שיכיל את הקובץ
+        XmlDocument XmlDocument;
+        //הכתובת במחשב שיאוחסן בו הקובץ
+        string pathName = Directory.GetCurrentDirectory() + "\\drinkData.xml";
+        public Form1()
+        {
+            InitializeComponent();
+            //יצירת מופע של XML
+            XmlDocument = new XmlDocument();
+            //אם קיים קובץ במיקום הזה
+            if (File.Exists(pathName))
+            {
+                try
+                {
+                    //טעינה של הקובץ
+                    XmlDocument.Load(pathName);
+                }
+                catch (Exception ex)
+                {
+                    //שגיעת טעינה
+                    MessageBox.Show("ישך' בעיה בטעינה" + ex.Message);
+                }
+            }
+            //אם לא קיים שם קובץ
+            else
+            {
+                //יצירת תגית ראשית לקובץ
+                XmlNode root = XmlDocument.CreateElement("Drinks");
+                //הוספת התגית לקובץ
+                XmlDocument.AppendChild(root);
+                //שמירת הקובץ במחשב
+                XmlDocument.Save(pathName);
+            }
+            //קראה לפונקציה שמציגה את הקובץ בגריד
+            ShowAllDrinks();
+        }
+        //יצירת משקה לפי מבנה מסוכם. מקבל את התגית הראשית כפרמטר
+        private bool CreateDrink(XmlNode root)
+        {
+            try
+            {
+                //יצירת תגית למשקה
+                XmlNode drink = XmlDocument.CreateElement("drink");
+                //הכנסת תגיות וערכים למשקה
+                drink.AppendChild(XmlDocument.CreateElement("name")).InnerText = txtNewDrinke.Text;
+                drink.AppendChild(XmlDocument.CreateElement("suger")).InnerText = cmbSugar.Text;
+                drink.AppendChild(XmlDocument.CreateElement("coffe")).InnerText = cmbCffe.Text;
+                drink.AppendChild(XmlDocument.CreateElement("coco")).InnerText = cmbCocou.Text;
+                drink.AppendChild(XmlDocument.CreateElement("milk")).InnerText = cmbMilk.Text;
+                drink.AppendChild(XmlDocument.CreateElement("price")).InnerText = txtPrice.Text;
+                //הכנסת המשקה לקובץ
+                root.AppendChild(drink);
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        //פונקציה בלחיצה על הוספת משקה
+        private void btnAddDrink_Click(object sender, EventArgs e)
+        {
+            //שליחה לפונקציה של היצירה ושליחת התגית הראשית של הקובץ
+            CreateDrink(XmlDocument.FirstChild);
+        }
+        //ניקוי התאים בעמוד
+        private void cleer()
+        {
+            foreach (Control ctr in Controls)
+            {
+                if (ctr is ComboBox || ctr is TextBox)
+                {
+                    ctr.Text = string.Empty;
+                }
+                txtPrice.Text = "0";
+            }
+        }
+        //פונקציה בלחיצה על הכפתור שמירה
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //שמירת הקובץ שיצרתי
+            XmlDocument.Save(pathName);
+            //ניקוי הדף
+            cleer();
+            //הצגת המשקאות
+            ShowAllDrinks();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            txtPrice.Text = (double.Parse(txtPrice.Text) + 0.5).ToString();
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        { 
+            if (txtPrice.Text == "0")
+            {
+                return;
+            }
+            txtPrice.Text = (double.Parse(txtPrice.Text) - 0.5).ToString();
+        }
+        //בלחיצה על כפתור המחיקה
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            foreach (XmlNode drink in XmlDocument.FirstChild.ChildNodes)
+            {
+                if (drink.SelectSingleNode("name").InnerText == txtNewDrinke.Text)
+                {
+                    XmlDocument.FirstChild.RemoveChild(drink);
+                }
+            }
+            ShowAllDrinks();
+        }       
+        //הצגת כל המשקאות
+        private void ShowAllDrinks()
+        {
+            dgvDrinksTable.Rows.Clear();
+            foreach (XmlNode drink in XmlDocument.FirstChild.ChildNodes)
+            {
+                string name = "", suger = "", coffee = "", cocoa = "", price = "", milk = "";
+                foreach (XmlNode node in drink.ChildNodes)
+                {
+                    switch (node.Name)
+                    {
+                        case "name":
+                            name = node.InnerText; break;
+                        case "suger":
+                            suger = node.InnerText; break;
+                        case "coffe":
+                            coffee = node.InnerText; break;
+                        case "coco":
+                            cocoa = node.InnerText; break;
+                        case "milk":
+                            milk = node.InnerText; break;
+                        case "price":
+                            price = node.InnerText; break;
+                    }
+                }
+                dgvDrinksTable.Rows.Add(name, suger, coffee, cocoa, milk, price+" ש''ח ");
+        }   }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            foreach (XmlNode drink in XmlDocument.FirstChild.ChildNodes)
+            {
+                if (drink.SelectSingleNode("name").InnerText == txtNewDrinke.Text)
+                {
+                    drink.SelectSingleNode("suger").InnerText = cmbSugar.Text;
+                    drink.SelectSingleNode("coffe").InnerText = cmbCffe.Text;
+                    drink.SelectSingleNode("milk").InnerText = cmbMilk.Text;
+                    drink.SelectSingleNode("coco").InnerText = cmbCocou.Text;
+                    drink.SelectSingleNode("price").InnerText = txtPrice.Text;
+                    ShowAllDrinks();
+                }
+
+            } 
+
+        }
+
+    }
+}
